f2859d9ca889286670172330d543f2f05fb59b21
Ashish Khare <akstyles211@gmail.com>
2026-05-02 14:42:56 +0530

    fix: Eurler class int and bool -> number and boolean

--- utils/url.ts
+++ utils/url.ts
@@ -1,17 +1,17 @@
 export class Euler {
 	base_url: string;
-	chunks: strin[];
+	chunks: string[];
 
 	constructor(url: string) {
 		this.base_url = url
 		this.chunks = url.split('/').slice(1)
 	}
 
-	get length(): int {
+	get length(): number {
 		return this.chunks.length;
 	}
 
-	isAt(index: int, phrase: string): bool {
+	isAt(index: number, phrase: string): boolean {
 		return this.chunks[index] == phrase;
 	}
 }
